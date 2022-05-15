1bb960869b2b987248298d98fb468bc265d1fde6
Pro-input <omar.a@input.ae>
2022-05-16 01:18:49 +0400

    [16-5] add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { RoutingRoutingModule } from './routing-routing.module';
+import { IconsModule } from './shared/icons/icons.module';
+
 import { HomeComponent } from './pages/home/home.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { CategoriesComponent } from './pages/categories/categories.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    NavbarComponent,
+    FooterComponent,
+    CategoriesComponent
   ],
   imports: [
     BrowserModule,
     RoutingRoutingModule,
-    NgbModule
+    NgbModule,
+    IconsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
