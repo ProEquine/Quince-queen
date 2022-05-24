c1c650d1a70c569812a14fbf4f9681a53a524376
Pro-input <omar.a@input.ae>
2022-05-24 10:24:56 +0400

    [change 2kb to 8kb and 4kb to 10kb ]

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 const path = require('path');
 const PORT = process.env.PORT || 5000;
-var app_path = '../dist/behalal/';
+var app_path = '../dist/quince-queen';
 
 app.use('/' , express.static(path.join(__dirname , app_path)))
 .get('*' , (req,res) => res.sendFile(path.join(__dirname, app_path + 'index.html')))
