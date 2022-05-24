78350a953ea4e2879f90010fc552570bc14ada5c
Pro-input <omar.a@input.ae>
2022-05-24 11:49:35 +0400

    add deploy to heroku

--- server/server.js
+++ server/server.js
@@ -7,3 +7,12 @@ var app_path = '../dist/quince-queen';
 app.use('/' , express.static(path.join(__dirname , app_path)))
 .get('*' , (req,res) => res.sendFile(path.join(__dirname, app_path + 'index.html')))
 .listen(PORT, ()=>console.log(`/Listening on ${PORT}`)); 
+
+/*
+https://medium.com/geekculture/how-to-easily-deploy-your-first-angular-app-on-heroku-65dd546c8181
+git init
+heroku git:remote -a quince-queen
+ git add .
+git commit -am "make it better"
+git push heroku main
+*/
