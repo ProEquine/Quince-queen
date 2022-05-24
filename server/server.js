6ed8bf710648d3f20d52f553b21d2aae39bbdbdb
Pro-input <omar.a@input.ae>
2022-05-24 10:26:47 +0400

    add server.js

--- server/server.js
+++ server/server.js
@@ -6,4 +6,4 @@ var app_path = '../dist/quince-queen';
 
 app.use('/' , express.static(path.join(__dirname , app_path)))
 .get('*' , (req,res) => res.sendFile(path.join(__dirname, app_path + 'index.html')))
-.listen(PORT, ()=>console.log(`/Listening on ${PORT}`));
+.listen(PORT, ()=>console.log(`/Listening on ${PORT}`)); 
