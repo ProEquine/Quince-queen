b6be208660c321f88441be8c9935c21345e1c51f
Pro-input <omar.a@input.ae>
2022-05-26 10:10:52 +0400

    [fix style En]

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -15,59 +15,59 @@ export class NavbarComponent implements OnInit {
   constructor(private translateService: TranslateService,
     @Inject(DOCUMENT) private document: Document,
     private viewportScroller: ViewportScroller
-    ) {
-        translateService.addLangs(['en', 'ar']);
-        translateService.setDefaultLang('en');
-    }
-  changeLangage(lang: string) {
-  this.translateService.setDefaultLang(lang);
-  this.translateService.use(lang);
-
-
-  let htmlTag = this.document.getElementsByTagName("html")[0] as HTMLHtmlElement;
-  htmlTag.dir = lang === "ar" ? "rtl" : "ltr";
-  this.translateService.setDefaultLang(lang);
-  this.translateService.use(lang);
-  this.changeCssFile(lang);
-  this.navbarCollapsed = true;
-  
-}
-changeCssFile(lang: string) {
-  let headTag = this.document.getElementsByTagName("head")[0] as HTMLHeadElement;
-  let existingLink = this.document.getElementById("langCss") as HTMLLinkElement;
-  let bundleName = lang === "ar" ? "styles.rtl.scss":"styles.scss";
-  if (existingLink) {
-     existingLink.href = bundleName;
-  } else {
-     let newLink = this.document.createElement("link");
-     newLink.rel = "stylesheet";
-     newLink.type = "text/css";
-     newLink.id = "langCss";
-     newLink.href = bundleName;
-     headTag.appendChild(newLink);
+  ) {
+    translateService.addLangs(['en', 'ar']);
+    translateService.setDefaultLang('en');
   }
+  changeLangage(lang: string) {
+    this.translateService.setDefaultLang(lang);
+    this.translateService.use(lang);
+
+
+    let htmlTag = this.document.getElementsByTagName("html")[0] as HTMLHtmlElement;
+    htmlTag.dir = lang === "ar" ? "rtl" : "ltr";
+    this.translateService.setDefaultLang(lang);
+    this.translateService.use(lang);
+    this.changeCssFile(lang);
+    this.navbarCollapsed = true;
+
+  }
+  changeCssFile(lang: string) {
+    let headTag = this.document.getElementsByTagName("head")[0] as HTMLHeadElement;
+    let existingLink = this.document.getElementById("langCss") as HTMLLinkElement;
+    let bundleName = lang === "ar" ? "styles.rtl.scss" : "styles.scss";
+    if (existingLink) {
+      existingLink.href = bundleName;
+    } else {
+      let newLink = this.document.createElement("link");
+      newLink.rel = "stylesheet";
+      newLink.type = "text/css";
+      newLink.id = "langCss";
+      newLink.href = bundleName;
+      headTag.appendChild(newLink);
+    }
   }
 
   ngOnInit(): void {
   }
-   /**
-    * Listens to window scroll events
-    *
-    * @param {*} event
-    * @memberof HeaderComponent
-    */
-   @HostListener('window:scroll', ['$event'])
-   onScroll(event:any) {
-      if (window.pageYOffset > 130) this.hasScrolledBanner = true;
-      else this.hasScrolledBanner = false;
-   }
+  /**
+   * Listens to window scroll events
+   *
+   * @param {*} event
+   * @memberof HeaderComponent
+   */
+  @HostListener('window:scroll', ['$event'])
+  onScroll(event: any) {
+    if (window.pageYOffset > 130) this.hasScrolledBanner = true;
+    else this.hasScrolledBanner = false;
+  }
 
-   scrollToElement(element:any): void {
-    element.scrollIntoView({behavior: "smooth", inline: "nearest"});
-}
-public onClick(elementId: string): void { 
-  this.viewportScroller.scrollToAnchor(elementId);
-  this.navbarCollapsed = true;
-}
+  scrollToElement(element: any): void {
+    element.scrollIntoView({ behavior: "smooth", inline: "nearest" });
+  }
+  public onClick(elementId: string): void {
+    this.viewportScroller.scrollToAnchor(elementId);
+    this.navbarCollapsed = true;
+  }
 
 }
